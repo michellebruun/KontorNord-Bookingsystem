35d59ca11a1fdb30dba18e80c732668f48e33d7b
Michelle <michellebruun97@gmail.com>
2026-03-09 12:07:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KontorNord_Bookingsystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
