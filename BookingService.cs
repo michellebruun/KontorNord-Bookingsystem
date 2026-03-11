d477f5767fa000fb16ed21baa7ddccca406f2b22
Frederik <FrederikPedersen55@outlook.com>
2026-03-11 12:20:33 +0100

    Fjernet meetingroom class

--- BookingService.cs
+++ BookingService.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace KontorNord_Bookingsystem
 {
